019bab08982d01750a7f5da7b4ce5194904ca6c2
root <root@LAPTOP-6VG4N4JG.localdomain>
2019-11-04 16:05:58 -0600

    fuck yeah

--- csim.c
+++ csim.c
@@ -1,7 +1,242 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <getopt.h>
+#include <stdlib.h>
 
-int main()
-{
-    printSummary(0, 0, 0);
+// Author: Zhihan Zhang
+// WUSTL ID: 478594
+
+enum {
+    TYPE_MODIFY,
+    TYPE_LOAD,
+    TYPE_SAVE
+};
+
+enum {
+    HIT,
+    MISS,
+    EVICTION
+};
+
+typedef struct {
+    int visible;
+    int set_num;
+    int entry;
+} cache_size;
+
+typedef struct {
+    unsigned long int valid;
+    unsigned long int tag;
+} cache_line;
+
+typedef struct {
+    char op;
+    unsigned long int addr;
+    unsigned long int index;
+    unsigned long int tag;
+    unsigned int mem_size;
+} operation;
+
+typedef struct {
+    int hits;
+    int misses;
+    int evictions;
+} summary;
+
+void Caching(cache_line *cache, operation an_op, cache_size c_size, summary *my_summary) {
+
+    int index;
+    int type;
+    int result;
+    unsigned long int temp_rag;
+
+    // get type
+    switch (an_op.op) {
+        case 'M':
+            type = TYPE_MODIFY;
+            break;
+        case 'L':
+            type = TYPE_LOAD;
+            break;
+        case 'S':
+            type = TYPE_SAVE;
+            break;
+    }
+    cache_line *pcache = cache + an_op.index * c_size.entry;
+
+    // get result type
+    result = EVICTION;
+    for (index = 0; index < c_size.entry; index++) {
+        if ((pcache + index)->valid == 0) {
+            result = MISS;
+            break;
+        }
+        if ((pcache + index)->tag == an_op.tag) {
+            result = HIT;
+            break;
+        }
+    }
+
+    // LRU policy
+    switch (result) {
+        // if HIT, just refrash the time
+        case HIT:
+            temp_rag = (pcache + index)->tag;
+            for ( ; index < c_size.entry - 1 && (pcache + index + 1)->valid == 1; index++) {
+                (pcache + index)->tag = (pcache + index + 1)->tag;
+            }
+            (pcache + index)->tag = temp_rag;
+            my_summary->hits++;
+            break;
+        // if MISS, add it to the cache
+        case MISS:
+            my_summary->misses++;
+            (pcache + index)->valid = 1;
+            (pcache + index)->tag = an_op.tag;
+            break;
+        // if EVICTION, delete the line who have minimum time and add it to the cache
+        case EVICTION:
+            my_summary->misses++;
+            my_summary->evictions++;
+            for (index = 0; index < c_size.entry - 1; index++) {
+                (pcache + index)->tag = (pcache + index + 1)->tag;
+            }
+            (pcache + index)->tag = an_op.tag;
+            break;
+    }
+
+    // the write in type modify is sure to hit
+    if (type == TYPE_MODIFY)
+        my_summary->hits++;
+        
+    // print type ( Only visible = 1 )
+    if (c_size.visible == 1) {
+        switch (type) {
+            case TYPE_MODIFY:
+                printf("M ");
+                break;
+            case TYPE_LOAD:
+                printf("L ");
+                break;
+            case TYPE_SAVE:
+                printf("S ");
+                break;
+        }
+        printf("%lx,%d ", an_op.addr, an_op.mem_size);
+        switch (result) {
+            case HIT:
+                printf("hit");
+                break;
+            case MISS:
+                printf("miss");
+                break;
+            case EVICTION:
+                printf("miss eviction");
+                break;
+        }
+        if (type == TYPE_MODIFY)
+            printf(" hit");
+        printf("\n");
+    }
+}
+
+
+void print_help_info(void) {
+    // Print the help inforation of this program
+    printf("Usage: ./csim [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+    printf("Options:\n");
+    printf("  -h         Print this help message.\n");
+    printf("  -v         Optional verbose flag.\n");
+    printf("  -s <num>   Number of set index bits.\n");
+    printf("  -E <num>   Number of lines per set.\n");
+    printf("  -b <num>   Number of block offset bits.\n");
+    printf("  -t <file>  Trace file.\n");
+    printf("\n");
+    printf("Examples:\n");
+    printf("  linux>  ./csim -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+    printf("  linux>  ./csim -v -s 8 -E 2 -b 4 -t traces/yi.trace\n");
+}
+
+int main(int argc, char **argv) {
+
+    // get options
+    char opt;
+    int help = 0, visible = 0, set_power = 0, set_num = 0, entry = 0, block_power = 0, trace = 0;
+    char *filepath;
+    while((opt = getopt(argc, argv, "hvs:E:b:t:")) != -1) {
+        switch (opt) {
+            case 'h':
+                help = 1;
+                break;
+            case 'v':
+                visible = 1;
+                break;
+            case 's':
+                set_power = atoi(optarg);
+                set_num = 1 << set_power;
+                break;
+            case 'E':
+                entry = atoi(optarg);
+                break;
+            case 'b':
+                block_power = atoi(optarg);
+                break;
+            case 't':
+                trace = 1;
+                filepath = optarg;
+                break;
+            default:
+                help = 1;
+                break;
+        }
+    }
+
+    // Print help info
+    if (help == 1) {
+        print_help_info();
+        exit(0);
+    }
+    if (set_power == 0 || entry == 0 || block_power == 0 || trace == 0) {
+        printf("Arguments Error!\n\n");
+        print_help_info();
+        exit(1);
+    }
+
+    // Build cache
+    summary my_summary;
+    cache_size c_size;
+    int index;
+    my_summary.hits = my_summary.misses = my_summary.evictions = 0;
+    c_size.visible = visible, c_size.set_num = set_num, c_size.entry = entry;
+    cache_line *cache = (cache_line *)malloc(sizeof(cache_line) * set_num * entry);
+    if (cache == NULL) {
+        printf("Memory Error!\n");
+        exit(1);
+    }
+    for (index = 0; index < set_num * entry; index++) {
+        (cache + index)->valid = 0;
+        (cache + index)->tag = 0xffffffff;
+    }
+
+    // Read file
+    FILE *trace_file = fopen(filepath, "r");
+    operation an_op;
+    char line[80];
+    char *pline = NULL;
+    while (fgets(line, 80, trace_file) != NULL) {
+        pline = line;
+        if (*pline++ == 'I')
+            continue;
+        sscanf(pline, "%c %lx,%u", &an_op.op, &an_op.addr, &an_op.mem_size);
+        an_op.index = (an_op.addr >> block_power) & ~(~0 << set_power);
+        an_op.tag = an_op.addr >> (block_power + set_power);
+        Caching(cache, an_op, c_size, &my_summary);
+    }
+    free(cache);
+
+    // Print answer
+    printSummary(my_summary.hits, my_summary.misses, my_summary.evictions);
     return 0;
 }
--- trans.c
+++ trans.c
@@ -22,8 +22,108 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
-}
+//32*32
+//4*64*(1/8 + 1/4) + 12*64*2*(1/8) = 288
+//Min "misses" is about 288 based on my calculation
+    int a1,a2,a3,a4,a5,a6,a7,a8;
+    int i,j,k,l,h;
 
+    if(M == 32 && N == 32){
+        for(i = 0; i < 4; i++){
+            for(j = 0; j < 4; j++){
+                for(k = i*8; k < (i+1)*8; k++){
+                    h=j*8;
+                    a1=A[k][h];
+                    a2=A[k][h+1];
+                    a3=A[k][h+2];
+                    a4=A[k][h+3];
+                    a5=A[k][h+4];
+                    a6=A[k][h+5];
+                    a7=A[k][h+6];
+                    a8=A[k][h+7];
+
+                    B[h][k]=a1;
+                    B[h+1][k]=a2;
+                    B[h+2][k]=a3;
+                    B[h+3][k]=a4;
+                    B[h+4][k]=a5;
+                    B[h+5][k]=a6;
+                    B[h+6][k]=a7;
+                    B[h+7][k]=a8;
+                }
+            }
+        }
+    }
+//64*64
+//Min "misses" is about 1180 based on my calculation
+    else if(M == 64 && N == 64) {
+        for (i = 0; i < 64; i += 8) {
+            for (j = 0; j < 64; j += 8) {
+                for (k = i; k < i + 4; k++) {
+                    a1 = A[k][j];
+                    a2 = A[k][j + 1];
+                    a3 = A[k][j + 2];
+                    a4 = A[k][j + 3];
+                    a5 = A[k][j + 4];
+                    a6 = A[k][j + 5];
+                    a7 = A[k][j + 6];
+                    a8 = A[k][j + 7];
+
+                    B[j][k] = a1;
+                    B[j + 1][k] = a2;
+                    B[j + 2][k] = a3;
+                    B[j + 3][k] = a4;
+                    B[j][k + 4] = a5;
+                    B[j + 1][k + 4] = a6;
+                    B[j + 2][k + 4] = a7;
+                    B[j + 3][k + 4] = a8;
+            }
+            for (l = j + 4; l < j + 8; l++) {
+
+                a5 = A[i + 4][l - 4];
+                a6 = A[i + 5][l - 4];
+                a7 = A[i + 6][l - 4];
+                a8 = A[i + 7][l - 4];
+
+                a1 = B[l - 4][i + 4];
+                a2 = B[l - 4][i + 5];
+                a3 = B[l - 4][i + 6];
+                a4 = B[l - 4][i + 7];
+
+                B[l - 4][i + 4] = a5;
+                B[l - 4][i + 5] = a6;
+                B[l - 4][i + 6] = a7;
+                B[l - 4][i + 7] = a8;
+
+                B[l][i] = a1;
+                B[l][i + 1] = a2;
+                B[l][i + 2] = a3;
+                B[l][i + 3] = a4;
+
+                B[l][i + 4] = A[i + 4][l];
+                B[l][i + 5] = A[i + 5][l];
+                B[l][i + 6] = A[i + 6][l];
+                B[l][i + 7] = A[i + 7][l];
+            }
+        }
+    }
+    }
+//61*67
+//Min "misses" is about 1900 based on my calculation
+//I ran test with different block size (4 - 30), and I found that "misses" is min when each block has size 23. 
+else if (M == 61 && N == 67){
+    for (i = 0; i < N; i += 23) {
+        for (j = 0;  j < M; j += 23) {
+            for (k = i; k < i + 23 && k < N; k++) {
+                for (l = j; l < j + 23 && l < M; l++) {
+                    h = A[k][l];
+                    B[l][k] = h;
+                }
+            }
+        }
+    }
+}
+}
 /* 
  * You can define additional transpose functions below. We've defined
  * a simple one below to help you get started. 
